5e3b0bd94576686db93edb0eb7853c01dbf62dfe
Jakub Jankiewicz <jcubic@onet.pl>
2017-06-18 16:59:18 +0200

    Add stage-0 babel preset

--- webpack.config.js
+++ webpack.config.js
@@ -68,7 +68,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015']
+                    presets: ['es2015', 'stage-0']
                 }
             },
             {
